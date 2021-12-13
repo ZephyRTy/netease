5821467c72761da4da0d68e8153990ad8023cd24
0xZephyR <1937176774@qq.com>
2021-12-13 22:06:19 +0800

    feat: 将cookie提取为全局变量；新增数据结构：当前显示歌曲列表

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,3 +1,17 @@
+import { makeAutoObservable, observable } from 'mobx';
+import { Song } from './song';
+
 export const realIP = '76.223.126.88';
 export const serverPath =
 	'https://netease-cloud-music-api-ol9f71wh4-zephyrty.vercel.app';
+export const cookie = {
+	value: '',
+	get() {
+		return this.value;
+	},
+	set(newV: string) {
+		this.value = newV;
+	}
+};
+const activeList = observable.array([] as Song[]);
+export const active = makeAutoObservable({ activeList, status: false });
