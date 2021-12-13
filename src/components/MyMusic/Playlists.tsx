01df04a08be302a7a96d91167946ec6955e13e33
0xZephyR <1937176774@qq.com>
2021-12-13 22:03:13 +0800

    feat: 实现显示歌单的歌曲列表，更新部分样式

--- src/components/MyMusic/Playlists.tsx
+++ src/components/MyMusic/Playlists.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { useState } from 'react';
+import { cookie } from '../../utils/global';
 import { PlayList } from '../../utils/playList';
 import { user } from '../../utils/user';
 import './style/playlist.scss';
@@ -12,15 +13,42 @@ const PlaylistItem = (props: { playlist: PlayList }) => {
 					className="playlist-item-cover"
 					src={props.playlist.coverImgUrl}
 				/>
+				<div className="playlist-item-info">
+					<span className="playlist-item-info-name">
+						<a
+							className="playlist-item-info-link"
+							href="javascript:void(0)"
+							onClick={() => {
+								props.playlist.getTracks(cookie.get());
+							}}
+						>
+							{props.playlist.name}
+						</a>
+					</span>
+					<span className="playlist-item-info-length">
+						{`${props.playlist.length}首`}
+					</span>
+				</div>
 			</div>
 		</li>
 	);
 };
-const Playlist = (props: { playlists: PlayList[] }) => {
+const Playlist = (props: { playlists: PlayList[]; title: string }) => {
 	const [open, setOpen] = useState(true);
 	return (
 		<div>
-			<span onClick={() => setOpen((v) => !v)}>title</span>
+			<div
+				className={
+					'playlist-title' + (open ? ' playlist-title-open' : '')
+				}
+			>
+				<span
+					className={'playlist-title-span'}
+					onClick={() => setOpen((v) => !v)}
+				>
+					{props.title}
+				</span>
+			</div>
 			<ul className={'playlist' + (open ? ' playlist-open' : '')}>
 				{props.playlists.map((v) => (
 					<PlaylistItem key={v.id} playlist={v} />
@@ -33,7 +61,16 @@ export const PlaylistsContainer = observer((props: { user: typeof user }) => {
 	return (
 		<div className="playlist-container">
 			{props.user._infoLoaded ? (
-				<Playlist playlists={props.user.playLists} />
+				<>
+					<Playlist
+						playlists={props.user.createdPlaylists}
+						title="创建的歌单"
+					/>
+					<Playlist
+						playlists={props.user.subPlaylists}
+						title="收藏的歌单"
+					/>
+				</>
 			) : null}
 		</div>
 	);
