b82d6cd97df3f73feaa881dd9a1a55ad8277ba83
0xZephyR <1937176774@qq.com>
2021-12-18 16:00:57 +0800

    feat: 用于获取相似歌曲和歌单的工具类

---
+++ src/utils/sim.ts
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import { cookie, realIP, serverPath } from './global';
+import { PlayList } from './playList';
+import { IAlbum, IArtist, Song } from './song';
+import { Track } from './track';
+
+export class SimUtil {
+	private constructor() {}
+	static getSim(obj: Track, mode: 'song' | 'playlist', setState: any) {
+		axios
+			.get(
+				`${serverPath}/simi/${mode}?id=${
+					obj.id
+				}&realIP=${realIP}&cookie=${cookie.get()}`
+			)
+			.then((res) => {
+				console.log(mode + ': ');
+				console.log(res);
+				setState(SimUtil.utils[mode as string](res.data[`${mode}s`]));
+			});
+	}
+	private static utils: { [song: string]: Function; playlist: Function } = {
+		song: (
+			o: {
+				name: string;
+				id: string;
+				album: IAlbum;
+				artists: IArtist[];
+				duration: number;
+			}[]
+		) => {
+			return o.map(
+				(obj) =>
+					new Song(
+						obj.name,
+						obj.id,
+						obj.album,
+						obj.artists,
+						obj.duration
+					)
+			);
+		},
+		playlist(
+			info: {
+				id: string;
+				name: string;
+				coverImgUrl: string;
+				trackCount: number;
+				creator: { nickname: string };
+			}[]
+		) {
+			return info.map((v) => new PlayList(v, cookie.get()));
+		}
+	};
+
+	static playlist(id: string) {
+		axios
+			.get(
+				`${serverPath}/simi/playlist?id=${id}&realIP=${realIP}&cookie=${cookie.get()}`
+			)
+			.then((res) => console.log(res));
+	}
+}
