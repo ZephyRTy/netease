328dcf0b0c16dc887cdb97488d2e231c379e68e0
0xZephyR <1937176774@qq.com>
2021-12-18 15:50:41 +0800

    feat: 总路由

---
+++ src/components/Router.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { User } from '../utils/user';
+import { MainContentLayout } from './MainContent/MainContent';
+import { MyMusicLayout } from './MyMusic/MyMusicLayout';
+import { TrackLayout } from './Track/TrackLayout';
+
+export const Router = (props: { user: User }) => {
+	return (
+		<HashRouter>
+			<Routes>
+				<Route
+					element={<MainContentLayout user={props.user} />}
+					path="/"
+				>
+					<Route
+						element={<MyMusicLayout user={props.user} />}
+						path="playlist/*"
+					/>
+					<Route element={<TrackLayout />} path="track/:id"></Route>
+				</Route>
+			</Routes>
+		</HashRouter>
+	);
+};
