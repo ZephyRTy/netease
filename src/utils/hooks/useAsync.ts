e649b4736de4220be3c564f46186ed1316093360
0xZephyR <1937176774@qq.com>
2021-12-19 21:54:20 +0800

    feat: 异步数据请求的custom hook

---
+++ src/utils/hooks/useAsync.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
+
+export function useAsync<T>(
+	fn: () => Promise<T | void> | undefined,
+	setState: React.Dispatch<React.SetStateAction<T>>,
+	deps: React.DependencyList,
+	replace?: any
+) {
+	useEffect(() => {
+		if (typeof replace !== 'undefined') setState(replace);
+		let flag = true;
+		fn()?.then((res) => {
+			if (flag) setState(res!);
+		});
+		return () => {
+			flag = false;
+		};
+	}, deps);
+}
