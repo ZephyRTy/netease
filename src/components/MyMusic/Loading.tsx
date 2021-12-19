f5ff8e31b6ffbcb5482aa8ebe9bb1d924ea7f6b4
0xZephyR <1937176774@qq.com>
2021-12-19 22:50:22 +0800

    修改依赖

--- src/components/MyMusic/Loading.tsx
+++ src/components/MyMusic/Loading.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { setInterval } from 'timers';
 import './style/loading.scss';
 export const Loading = () => {
 	const [count, setCount] = useState(0);
