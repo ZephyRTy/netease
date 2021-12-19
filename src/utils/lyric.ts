556abb3f733bd547d299bad48374d4af5c245c4c
0xZephyR <1937176774@qq.com>
2021-12-19 21:53:40 +0800

    feat: 歌词随音乐播放滚动，并聚焦到当前歌词

--- src/utils/lyric.ts
+++ src/utils/lyric.ts
@@ -1,45 +1,65 @@
 import axios from 'axios';
-import React from 'react';
+import { action, makeObservable, observable } from 'mobx';
 import { realIP, serverPath } from './global';
+import { Timer } from './timer';
 export class LyricItem {
-	private _timeStamp = '';
+	private _timeStamp = 0;
 	private _content = '';
+	focused = false;
+	pos = 0;
 	constructor(content: string) {
+		makeObservable(this, {
+			focused: observable,
+			focus: action,
+			unfocus: action
+		});
 		let v = content.split(']');
 		this._content = v[1];
-		this._timeStamp = v[0].slice(1);
+		const t = v[0].slice(1).split(':');
+		this._timeStamp = parseInt(t[0]) * 60 + Math.round(parseFloat(t[1]));
+		//console.log(this._timeStamp);
 	}
 
 	get content() {
 		return this._content;
 	}
+
+	get time() {
+		return this._timeStamp;
+	}
+
+	focus() {
+		this.focused = true;
+	}
+	unfocus() {
+		this.focused = false;
+	}
 }
 
 export class Lyric {
-	private _lyrics = [] as LyricItem[];
-	private _absolute = false;
-	getLyric(setState: React.Dispatch<React.SetStateAction<any>>, id: string) {
-		axios
-			.get(`${serverPath}/lyric?id=${id}&realIP=${realIP}`)
-			.then((res) => {
-				const lyric: string[] = res.data.lrc.lyric
-					.split('\n')
-					.slice(0, -1);
-				if (lyric.includes('[99:00.00]纯音乐，请欣赏')) {
-					this._absolute = true;
-					this._lyrics.push(
-						new LyricItem('[99:00.00]纯音乐，请欣赏')
-					);
-				} else {
-					this._lyrics = lyric.map((v) => {
-						return new LyricItem(v);
-					});
-				}
-				setState(this);
+	private lyrics = [] as LyricItem[];
+	async getLyric(id: string) {
+		const res = await axios.get(
+			`${serverPath}/lyric?id=${id}&realIP=${realIP}`
+		);
+		const lyric: string[] = res.data.lrc.lyric.split('\n').slice(0, -1);
+		if (lyric.includes('[99:00.00]纯音乐，请欣赏')) {
+			this.lyrics.push(new LyricItem('[99:00.00]纯音乐，请欣赏'));
+		} else {
+			this.lyrics = lyric.map((v) => {
+				return new LyricItem(v);
 			});
+		}
+		return this;
+	}
+	item(index: number) {
+		return this.lyrics[index];
 	}
 
+	mount(div: HTMLDivElement) {
+		return new Timer(this.lyrics, div);
+	}
 	get traverse() {
-		return [...this._lyrics];
+		return [...this.lyrics];
 	}
 }
