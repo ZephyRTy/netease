bad50549cb6dbffd51dae0776c25d85353759f72
justinxiang <liao1120x@gmail.com>
2021-12-22 22:10:24 +0800

    完善了navbar 走马灯和热门推荐的UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.scss';
 import { Router } from './components/Router';
-import { user } from './utils/obj/user';
 
 function App() {
-	return <Router user={user} />;
+	return <Router />;
 }
 export default App;
--- src/components/Discovery/HotRecommend/HotRecommend.tsx
+++ src/components/Discovery/HotRecommend/HotRecommend.tsx
@@ -1,34 +1,52 @@
-import React, { useEffect } from 'react';
-import { cookie, realIP, serverPath } from '../../../utils/global';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { realIP, serverPath } from '../../../utils/global';
+import { TitleTheme } from '../TitleTheme/TitleTheme';
+import { SongCover } from '../SongCover/SongCover';
 export const HotRecommend = () => {
-	let hotrecommend: any;
+	
+	const [hotRecommend, sethotRecommend] = useState([]);
+
 	useEffect(() => {
 		const url = `${serverPath}/personalized?realIP=${realIP}`;
-		fetch(url, {
-			method: 'GET'
-		})
-			.then((res) => {
-				return res.json();
-			})
-			.then((data) => {
-				hotrecommend = data['result'];
-			})
-			.catch((err) => console.log(err));
-	});
+		const fetchData = async () => {
+			const result = await axios(url);
+			sethotRecommend(result.data['result'])
+		};
+		fetchData();
+	}, []);
+	console.log(hotRecommend)
+	// useEffect(() => {
+	// 	const url = `${serverPath}/personalized?realIP=${realIP}`;
+	// 	fetch(url, {
+	// 		method: 'GET'
+	// 	})
+	// 		.then((res) => {
+	// 			return res.json();
+	// 		})
+	// 		.then((data) => {
+	// 			console.log(data)
+	// 			hotrecommend = data['result'];
+	// 		})
+	// 		.catch((err) => console.log(err));
+	// });
 
 	return (
-		<div>
-			<h1>热门推荐</h1>
-			<div>
-				{hotrecommend &&
-					hotrecommend.map((item: any, index: number) => {
-						return (
-							<div key={index}>
-								<h1>{item.name}</h1>
-								<img alt={item.name} src={item.picUrl} />
-							</div>
-						);
-					})}
+		<div className="HotRecommendWrapper">
+			<TitleTheme
+				keywords={['华语', '流行', '摇滚', '民谣', '电子']}
+				keywordsClick={undefined}
+				right={undefined}
+				showIcon={undefined}
+				title="热门推荐"
+			/>
+			<div className="HotRecommendWrapper-recommend-list">
+				{hotRecommend && hotRecommend.map((item: any) => {
+					return <SongCover info={item} key={item.id} songList={''}
+width={140}
+					       />;
+				}
+				)}
 			</div>
 		</div>
 	);
